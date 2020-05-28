654d9c96b77224ae13e355ab2cadb1534f3b3ae3
Samothui <23310124+samothui@users.noreply.github.com>
2020-05-28 18:32:42 +0300

    added clean version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TableComponent } from './table/table.component';
 import { SortableHeaderDirective} from './Directives/sortable-header.directive';
 import { TitleCasePipe, LowerCasePipe } from '@angular/common';
 
 import {NgxPaginationModule} from 'ngx-pagination'; // <--- external package
 import { ReactiveTableComponent } from './reactive-table/reactive-table.component';
 import { FormArrayFilterPipe } from './form-array-filter-pipe.pipe';
+import { HttpClientModule} from '@angular/common/http'
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent,
     SortableHeaderDirective,
     ReactiveTableComponent,
     FormArrayFilterPipe
@@ -24,7 +24,8 @@ import { FormArrayFilterPipe } from './form-array-filter-pipe.pipe';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    HttpClientModule,
   ],
   providers: [TitleCasePipe, LowerCasePipe],
   bootstrap: [AppComponent]
